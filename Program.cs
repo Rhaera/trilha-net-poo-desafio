7264c199671fa3ffef9620c897a5a9fc716d0d87
Rhaera <rauldantas@usp.br>
2023-01-27 23:36:18 -0300

    Yes

--- Program.cs
+++ Program.cs
@@ -1,10 +1,14 @@
 ﻿using DesafioPOO.Models;
 
 // TODO: Realizar os testes com as classes Nokia e Iphone
-Console.WriteLine("Attempt");
 
-Iphone iphone1 = new Iphone("(00) 9 0000-0000", "Iphone 10", "000000-00-000000-1", 128);
-Nokia nokia1   = new Nokia("(00) 9 0000-0000", "Nokia Special", "000000-00-000000-2", 64);
+Iphone iphone10 = new Iphone("(00) 9 0000-0000", "Iphone 10", "000000-00-000000-1", 128);
+Nokia nokiaSpecial   = new Nokia("(00) 9 0000-0000", "Nokia Special", "000000-00-000000-2", 64);
 
-nokia1.InstalarAplicativo("Rha&Ra");
-nokia1.ReceberLigacao();
+iphone10.InstalarAplicativo("Rha&Ra");
+iphone10.ReceberLigacao();
+iphone10.Ligar();
+
+nokiaSpecial.InstalarAplicativo("Rha&Ra");
+nokiaSpecial.ReceberLigacao();
+nokiaSpecial.Ligar();
